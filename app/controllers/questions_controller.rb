3ba8fd0184b0b0131b23f8477f1c936370c3ab33
sandy smircic <sandy.smircic@hotmail.com>
2020-05-11 14:47:48 +0100

    Stupid Coach

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    # raise
+    @question = params[:question]
+    if @question.include?('?')
+      @answer = 'Silly question, get dressed and go to work!'
+    elsif @question == 'I am going to work'
+      @answer = 'Great!'
+    elsif
+        @answer = "I don't care, get dressed and go to work!"
+    end
+  end
+end
+
+#create an if statement which will define a variable called answer which we can display
